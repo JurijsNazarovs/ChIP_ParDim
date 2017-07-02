2099a16c1fd82e98135eb55f721ea951aa8bbec8
Jurijs Nazarovs <nazarovs@wisc.edu>
2017-07-02 18:27:48 -0500

    trimmming dag maker and executed file

---
+++ ParDimLibs/exeTrim.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+#===============================================================================
+# This script trims files based on common mimum length or set length
+#
+# Input: strings with names of files joined by comma
+#       - repName
+#	- ctlName
+#	- dnaseName
+# Output: trimmed files	
+#===============================================================================
+## Libraries and options
+shopt -s nullglob #allows create an empty array
+shopt -s extglob #to use !
+homePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+source "$homePath"/funcList.sh #need to transfer
+
+curScrName=${0##*/} #delete last backSlash
+trimSoft="Trimmomatic-0.36/trimmomatic-0.36.jar"
+EchoLineBold
+echo "[Start] $curScrName"
+
+TrimSE(){
+  local fileInp=$1
+  local trimLen=$2
+  local isOrigName=${3:-"false"}
+  local resDir=${4:-"."}
+  
+  local fileTmp="$(mktemp -uq tmp.XXX)"
+
+  java -jar "$trimSoft" SE "$fileInp" "$fileTmp"\
+       CROP:"$trimLen"
+
+  if [[ $? -ne 0 ]]; then
+      ErrMsg "$fileInp was not trimmed.
+             Process is stopped."
+      if [[ -f "$fileTmp"  ]]; then
+          rm -rf "$fileTmp"
+      fi
+  else
+    mkdir -p "$resDir"
+    
+    if [[ "$isOrigName" = true ]]; then
+        strTmp="$resDir/$(basename "$fileInp")"
+        echo "$strTmp"
+        mv "$fileTmp" "$strTmp"
+    else
+      readarray -t strTmp <<< "$(echo "$fileInp" | tr "." "\n")"
+      strTmp=($(JoinToStr "." "${strTmp[0]}" trim "${strTmp[@]:1}"))
+      strTmp="$resDir/$(basename "$strTmp")"
+      echo "$strTmp"
+      mv "$fileTmp" "$strTmp"
+    fi
+
+    if [[ $? -ne 0 ]]; then
+        ErrMsg "Not able to move $fileTmp to $strTmp"
+    fi
+  fi
+}
+
+
+## Input and default values
+repName=$1
+ctlName=$2
+dnaseName=$3
+trimLen=$4
+isOrigName=${5:-"false"}
+resDir=${6:-"trimResDir"}
+outTar=${7:-"isPool.tar.gz"} #tarFile to return back on submit machine
+softZip=${8:-"Trimmomatic-0.36.zip"}
+isDry=${9:-true}
+
+posTypes=(rep ctl dnase)
+
+
+## Detect names and number of files for every type
+for i in "${posTypes[@]}"; do
+  eval "readarray -t tmpName <<< \"\$(echo \$${i}Name | tr \",\" \"\n\")\""
+  if [[ -z $(RmSp "$tmpName") ]]; then
+      eval "${i}Num=0"
+  else
+    for j in "${tmpName[@]}"; do
+      ChkExist f "$j" "Input file: $j\n"
+    done
+    
+    eval "${i}Num=${#tmpName[@]}"
+    eval "${i}Name=(\"\${tmpName[@]}\")"
+  fi
+done
+
+
+## Main part - trimming
+unzip "$softZip"
+if [[ $? -ne 0 ]]; then
+    ErrMsg "Cannot unzip $softZip"
+else
+  rm -rf "$softZip"
+fi
+
+if [[ -n "$trimLen" ]]; then
+    for i in "${repName[@]}" "${ctlName[@]}" "${dnaseName[@]}"; do
+      if [[ -z "$i" ]]; then
+          continue
+      fi
+      
+      TrimSE "$i" "$trimLen" "$isOrigName" "$resDir"
+    done
+else
+  # Detect the trimming length
+  EchoLineSh
+  echo "Detecting the trimming length ..."
+  echo ""
+  for i in "${posTypes[@]}"; do
+    eval "tmpName=(\"\${${i}Name[@]}\")"
+    
+    if [[ -z "$tmpName" ]]; then
+        continue
+    fi
+
+    for j in "${!tmpName[@]}"; do
+      fileTmp="${tmpName[j]}"
+      extTmp="${fileTmp##*.}"
+      
+      minLenTmp=$(awk  'NR==2 {print length($0); exit}' "$fileTmp")
+      minLenTmp=$((if [[ "$extTmp" = gz ]]; then
+                       gunzip -c "$fileTmp";
+                   else
+                     cat "$fileTmp";
+                   fi)|\
+                      awk -v minLen=minLenTmp\
+                      'NR%4 == 2 {if (length($0) < minLen){minLen = length($0)}}
+                      END{print minLen}')
+
+      eval "${i}MinLen[$j]=$minLenTmp"
+      echo "Min of $i$((j+1)): $minLenTmp bp"
+    done
+  done
+  echo "Done!"
+
+  # Copy length in case of several rep for an easy use
+  if [[ $repNum -gt 1 && $ctlNum -eq 1 ]]; then
+      for ((i=1; i<$repNum; i++)); do
+        ctlName[$i]="${ctlName[0]}"
+        ctlMinLen[$i]=${ctlMinLen[0]}
+      done
+  fi
+
+  # Trimming
+  echo "Trimming ..."
+  if [[ $repNum -gt 0 && $ctlNum -gt 0 ]]; then
+      for ((i=0; i<$repNum; i++)); do
+        trimLenTmp=$(Min ${repMinLen[$i]} ${ctlMinLen[$i]})
+        TrimSE "${repName[$i]}" "$trimLenTmp" "$isOrigName" "$resDir"
+        TrimSE "${ctlName[$i]}" "$trimLenTmp" "$isOrigName" "$resDir"
+      done
+  fi
+
+  if [[ $repNum -gt 0 && $ctlNum -eq 0 ]]; then
+      for ((i=0; i<$repNum; i++)); do
+        TrimSE "${repName[$i]}" "${repMinLen[$i]}" "$isOrigName" "$resDir"
+      done
+  fi
+
+  if [[ $ctlNum -gt 0 && $repNum -eq 0 ]]; then
+      for ((i=0; i<$ctlNum; i++)); do
+        TrimSE "${ctlName[$i]}" "${ctlMinLen[$i]}" "$isOrigName" "$resDir"
+      done
+  fi
+
+  if [[ $dnaseNum -gt 0 ]]; then
+      for ((i=0; i<$dnaseNum; i++)); do
+        TrimSE "${dnaseName[$i]}" "${dnaseMinLen[$i]}" "$isOrigName" "$resDir"
+      done
+  fi
+  echo "Done!"
+fi
+
+
+## Prepare tar to move results back
+tar -czf "$outTar" "$resDir"
+if [[ $? -ne 0 ]]; then
+    ErrMsg "Cannot create a $outTar"
+fi
+
+
+# Has to hide all unnecessary files in tmp directories
+if [[ "$isDry" = false ]]; then
+    mv !("$resDir") "$resDir"
+    mv "$resDir"/_condor_std* ./
+    mv "$resDir/$outTar" ./
+fi
+
+echo "[End]  $curScrName"
+EchoLineBold
+exit 0
---
+++ ParDimLibs/makeTrimDag.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+#===============================================================================
+# This script creates a right version of a dag file to trim files
+#
+# 2 possible ways to read input files: nested - rep/ctl dirs, not nested - all
+# files are in 1 dir.
+# 
+# Input:
+#	- argsFile	 file with all arguments for this shell		
+#==============================================================================
+## Libraries and options
+shopt -s nullglob #allows create an empty array
+homePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
+source "$homePath"/funcList.sh
+
+curScrName=${0##*/} #delete last backSlash
+
+
+## Input and default values
+argsFile=${1:-"args.listDev"} 
+dagFile=${2:-"Trim.dag"} #create this
+jobsDir=${3:-"TrimTmp"} #working directory, provided with one of analysed dirs
+resPath=${4:-"/tmp/Trim"} #return here on submit server. Read from file if empty
+inpDataInfo=${5} #text file with input data
+resDir=${6:-"TrimResDir"}
+transOut=${7:-"Trim"}
+
+
+## Default values, which can be read from the $argsFile
+posArgs=("isInpNested"
+         "exePath"
+         "inpExt"
+         "inpType"
+         "trimLen"
+         "isOrigName")
+
+isInpNested="true" #inside rep/ctl dirs or not
+exePath="$homePath/exeTrim.sh"
+inpExt="fastq.gz"
+inpType="rep, ctl"
+trimLen=""
+isOrigName=true
+softZip="Trimmomatic-0.36.zip"
+
+if [[ -z $(RmSp "$resPath") ]]; then
+    posArgs=("${posArgs[@]}" "resPath")
+fi
+
+ReadArgs "$argsFile" "1" "${curScrName%.*}" "${#posArgs[@]}" "${posArgs[@]}"\
+         > /dev/null
+if [[ "${resPath:0:1}" != "/" ]]; then
+    ErrMsg "The full path for resPath has to be provided.
+           Current value is: $resPath ."
+fi
+
+PrintArgs "$curScrName" "${posArgs[@]}" "jobsDir"
+
+
+## Initial checking
+ChkValArg "isInpNested" "" "true" "false"
+ChkValArg "isOrigName" "" "true" "false"
+if [[ -n $(RmSp "$trimLen") && ! "$trimLen" =~ ^[0-9]+$ ]] ; then
+    ErrMsg "The trimming length has to be an integer number or empty"
+fi
+WarnMsg "The input has to be in fastq format, despite the extension"
+
+readarray -t inpType <<<\
+          "$(awk\
+            '{ gsub(/,[[:space:]]*/, "\n"); print }' <<< "$inpType"
+           )"
+
+for i in "${inpType[@]}"; do
+  ChkValArg "i" "Input type\n" "rep" "ctl" "dnase"
+done
+
+
+## Detect reps and ctls
+DetectInput "$inpDataInfo" "${#inpType[@]}" "${inpType[@]}" "$inpExt"\
+            "$isInpNested" "true"
+
+if !([[ "$ctlNum" -eq 0 || "$ctlNum" -eq 1 || "$ctlNum" -eq "$repNum" || \
+      "$repNum" -eq 0 ]]); then
+    ErrMsg "Confusing number of ctl files.
+            Number of ctl: $ctlNum
+            Number of rep: $repNum"
+fi
+
+if [[ "$ctlNum" -eq 0 && "$repNum" -eq 0 && "$dnaseNum" -eq 0 ]]; then
+    ErrMsg "No input is detected"
+fi
+
+
+requirSize=0
+maxSize=0
+for i in "${repSize[@]}" "${ctlSize[@]}" "${dnaseSize[@]}"; do
+  requirSize=$((requirSize + i))
+  maxSize=$(Max $maxSize $i) # + this, since we create new trimmed file
+done
+
+
+## Condor
+# Calculate required memory, based on input files
+hd=$((requirSize + maxSize)) #size in bytes.
+hd=$((hd*1)) #increase size in 1 times
+hd=$(echo $hd/1024^3 + 1 | bc) #in GB rounded to a bigger integer
+hd=$((hd + 3)) #+1gb for safety + 2gb for software
+
+# Arguments for condor job
+argsCon=("\$(repName)"
+         "\$(ctlName)"
+         "\$(dnaseName)"
+         "$trimLen"
+         "$isOrigName"
+         "$resDir"
+         "\$(transOut)"
+         "$softZip"
+         "false")
+argsCon=$(JoinToStr "\' \'" "${argsCon[@]}")
+
+# Output directory for condor log files
+conOut="$jobsDir/conOut"
+mkdir -p "$conOut"
+
+# Transfered files
+transFiles=("${repName[@]}"
+            "${ctlName[@]}"
+            "${dnaseName[@]}"
+            "${exePath%/*}"/funcList.sh
+            "http://proxy.chtc.wisc.edu/SQUID/nazarovs/$softZip")
+transFiles="$(JoinToStr ", " "${transFiles[@]}")"
+
+# Main condor file
+conFile="$jobsDir/${curScrName%.*}.condor"
+bash "$homePath"/makeCon.sh "$conFile" "$conOut" "$exePath"\
+     "$argsCon" "$transFiles"\
+     "1" "1" "$hd" "\$(transOut)" "\$(transMap)"
+
+
+## Dag file
+printf "" > "$dagFile"
+jobId="Trim"
+printf "JOB  $jobId $conFile\n" >> "$dagFile"
+
+printf "VARS $jobId repName=\"$(JoinToStr "," "${repName[@]##*/}")\"\n"\
+       >> "$dagFile"
+printf "VARS $jobId ctlName=\"$(JoinToStr "," "${ctlName[@]##*/}")\"\n"\
+       >> "$dagFile"
+printf "VARS $jobId dnaseName=\"$(JoinToStr "," "${dnaseName[@]##*/}")\"\n"\
+       >> "$dagFile"
+
+
+printf "VARS $jobId transOut=\"$transOut.tar.gz\"\n" >> "$dagFile"
+printf "VARS $jobId transMap=\"\$(transOut)=$resPath/\$(transOut)\"\n"\
+       >> "$dagFile"
